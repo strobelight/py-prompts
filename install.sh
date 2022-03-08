68044c96361549afcb803660062a7b485fe63ccb
Ken Shaffer <kenneth_shaffer2@comcast.com>
2022-03-08 11:36:10 -0500

    add -e to echo to render color; change dark blue to cyan in simple color theme

--- install.sh
+++ install.sh
@@ -1,35 +1,41 @@
-#!/bin/sh
+#!/bin/bash
 
 ROOT_DIR=$HOME/.py-prompts
 rm -rf $ROOT_DIR
 
 rc_add() {
   name=$1
-  echo ".bashrc detected. updating \033[1;33mPATH\033[0m variable and exporting it through $HOME/$name"
-  echo "export PATH=$PATH:$HOME/.py-prompts" >> $HOME/$name
-  echo "\033[1;33mpy-prompts\033[0m is now installed on your machine."
+  echo -e ".bashrc detected. updating \033[1;33mPATH\033[0m variable and exporting it through $HOME/$name"
+  echo -e "export PATH=$PATH:$(pwd)/.py-prompts" >> $HOME/$name
+  echo -e "\033[1;33mpy-prompts\033[0m is now installed on your machine."
 }
 
-echo "Cloning repository: \033[1;33mhttps://github.com/arpitbbhayani/py-prompts\033[0m at $HOME/.bashrc"
-git clone https://github.com/arpitbbhayani/py-prompts $ROOT_DIR 2> /dev/null
+if [ -d themes ]; then
+    echo -e "Copy themes to $ROOT_DIR/themes"
+    mkdir -p $ROOT_DIR
+    cp -R themes $ROOT_DIR
+else
+    echo -e "Cloning repository: \033[1;33mhttps://github.com/arpitbbhayani/py-prompts\033[0m at $HOME/.bashrc"
+    git clone https://github.com/arpitbbhayani/py-prompts $ROOT_DIR 2> /dev/null
+fi
 
 if [ -f $HOME/.bashrc ]; then
   grep -q ".py-prompts" $HOME/.bashrc
   if [ $? -ne 0 ]; then
     rc_add .bashrc
   else
-    echo "\033[0;34mpy-prompts is already installed.\033[0m"
+    echo -e "\033[0;34mpy-prompts is already installed.\033[0m"
   fi
 elif [ -f $HOME/.zshrc ]; then
   grep -q ".py-prompts" $HOME/.zshrc
   if [ $? -ne 0 ]; then
     rc_add .zshrc
   else
-    echo "\033[0;34mpy-prompts is already installed.\033[0m"
+    echo -e "\033[0;34mpy-prompts is already installed.\033[0m"
   fi
 else
   echo ""
-  echo "\033[0;31mCould not detect your rc file. kindly add the following line in your rc file.\033[0m"
+  echo -e "\033[0;31mCould not detect your rc file. kindly add the following line in your rc file.\033[0m"
   echo ""
-  echo "\033[0;34mexport PATH=$PATH:$HOME/.py-prompts\033[0m"
+  echo -e "\033[0;34mexport PATH=$PATH:$HOME/.py-prompts\033[0m"
 fi
